a058d18c42c4cc0ee3f56d8198ab63e5f7a8c6f0
caleb <caleb@syndicate.io>
2025-07-15 18:10:13 -0500

    runOnce + working PKP

--- action.ts
+++ action.ts
@@ -1,162 +1,189 @@
 // @ts-nocheck
 const _litActionCode = async () => {
-  try {
-    // 1. Fetch drand randomness
-    const url =
-      "https://api.drand.sh/v2/chains/52db9ba70e0cc0f6eaf7803dd07447a1f5477735fd3f661792ba94600c84e971/rounds/latest"
-    const resp = await fetch(url).then((response) => response.json())
-    const { signature } = resp
-    if (!signature) {
-      Lit.Actions.setResponse({
-        response: "No signature found"
-      })
-      return
-    }
+	try {
+		const randomness = await Lit.Actions.runOnce(
+			{ waitForResponse: true, name: "randomnessGenerator" },
+			async () => {
+				try {
+					// 1. Fetch drand randomness
+					const url =
+						"https://api.drand.sh/v2/chains/52db9ba70e0cc0f6eaf7803dd07447a1f5477735fd3f661792ba94600c84e971/rounds/latest";
+					const resp = await fetch(url).then((response) => response.json());
+					const { signature } = resp;
+					if (!signature) {
+						Lit.Actions.setResponse({
+							response: "No signature found",
+						});
+						return;
+					}
 
-    // 2. Process drand signature
-    const signatureBytes = hexToBytes(signature)
-    const hashBuffer = await crypto.subtle.digest("SHA-256", signatureBytes)
-    const hashArray = Array.from(new Uint8Array(hashBuffer))
-    const drandRandomness = hashArray
-      .map((b) => b.toString(16).padStart(2, "0"))
-      .join("")
+					// 2. Process drand signature
+					const signatureBytes = hexToBytes(signature);
+					const hashBuffer = await crypto.subtle.digest(
+						"SHA-256",
+						signatureBytes,
+					);
+					const hashArray = Array.from(new Uint8Array(hashBuffer));
+					const drandRandomness = hashArray
+						.map((b) => b.toString(16).padStart(2, "0"))
+						.join("");
 
-    // 3. Generate local randomness using Web Crypto API
-    const localRandomBytes = crypto.getRandomValues(new Uint8Array(32))
-    const localRandomHex = Array.from(localRandomBytes)
-      .map((b) => b.toString(16).padStart(2, "0"))
-      .join("")
+					// 3. Generate local randomness using Web Crypto API
+					const localRandomBytes = crypto.getRandomValues(new Uint8Array(32));
+					const localRandomHex = Array.from(localRandomBytes)
+						.map((b) => b.toString(16).padStart(2, "0"))
+						.join("");
 
-    // 4. Combine both randomness sources using HMAC
-    const combinedData = new TextEncoder().encode(
-      drandRandomness + localRandomHex
-    )
-    const key = await crypto.subtle.importKey(
-      "raw",
-      new TextEncoder().encode("drand-local-combiner"),
-      { name: "HMAC", hash: "SHA-256" },
-      false,
-      ["sign"]
-    )
-    const combinedSignature = await crypto.subtle.sign(
-      "HMAC",
-      key,
-      combinedData
-    )
-    const combinedRandomness = Array.from(new Uint8Array(combinedSignature))
-      .map((b) => b.toString(16).padStart(2, "0"))
-      .join("")
+					// 4. Combine both randomness sources using HMAC
+					const combinedData = new TextEncoder().encode(
+						drandRandomness + localRandomHex,
+					);
+					const key = await crypto.subtle.importKey(
+						"raw",
+						new TextEncoder().encode("drand-local-combiner"),
+						{ name: "HMAC", hash: "SHA-256" },
+						false,
+						["sign"],
+					);
+					const combinedSignature = await crypto.subtle.sign(
+						"HMAC",
+						key,
+						combinedData,
+					);
+					const combinedRandomness = Array.from(
+						new Uint8Array(combinedSignature),
+					)
+						.map((b) => b.toString(16).padStart(2, "0"))
+						.join("");
 
-    // 5. Final entropy extraction using HKDF
-    const salt = new TextEncoder().encode("proof-of-survival-entropy")
-    const info = new TextEncoder().encode("combined-randomness")
+					// 5. Final entropy extraction using HKDF
+					const salt = new TextEncoder().encode("proof-of-survival-entropy");
+					const info = new TextEncoder().encode("combined-randomness");
 
-    const hkdfKey = await crypto.subtle.importKey(
-      "raw",
-      hexToBytes(combinedRandomness),
-      { name: "HKDF" },
-      false,
-      ["deriveBits"]
-    )
-    const finalRandomness = await crypto.subtle.deriveBits(
-      {
-        name: "HKDF",
-        hash: "SHA-256",
-        salt: salt,
-        info: info
-      },
-      hkdfKey,
-      256 // 32 bytes
-    )
-    const finalRandomnessHex = `0x${Array.from(new Uint8Array(finalRandomness))
-      .map((b) => b.toString(16).padStart(2, "0"))
-      .join("")}`
+					const hkdfKey = await crypto.subtle.importKey(
+						"raw",
+						hexToBytes(combinedRandomness),
+						{ name: "HKDF" },
+						false,
+						["deriveBits"],
+					);
+					const finalRandomness = await crypto.subtle.deriveBits(
+						{
+							name: "HKDF",
+							hash: "SHA-256",
+							salt: salt,
+							info: info,
+						},
+						hkdfKey,
+						256, // 32 bytes
+					);
+					const finalRandomnessHex = `0x${Array.from(
+						new Uint8Array(finalRandomness),
+					)
+						.map((b) => b.toString(16).padStart(2, "0"))
+						.join("")}`;
 
-    // Tx creation
-    const ethersProvider = new ethers.providers.JsonRpcProvider(RPC_URL)
-    const PKPETHAddress = ethers.utils.computeAddress(PKP_PUBLIC_KEY)
+					return finalRandomnessHex;
+				} catch (error) {
+					return `Error: Could not generate randomness: ${error.message}`;
+				}
+			},
+		);
+		console.log("randomness", randomness);
 
-    const abi = ["function setRandom(uint256 _random) external"]
-    const iface = new ethers.utils.Interface(abi)
-    const contract = new ethers.Contract(
-      RANDOM_CONTRACT_ADDRESS,
-      abi,
-      ethersProvider
-    )
-    const data = iface.encodeFunctionData("setRandom", [
-      BigInt(finalRandomnessHex)
-    ])
+		const unsignedTransactionResponse = await Lit.Actions.runOnce(
+			{ waitForResponse: true, name: "txnSender" },
+			async () => {
+				try {
+					// Tx creation
+					const ethersProvider = new ethers.providers.JsonRpcProvider(RPC_URL);
+					const PKPETHAddress = ethers.utils.computeAddress(PKP_PUBLIC_KEY);
+					const abi = ["function setRandom(uint256 _random) external"];
+					const iface = new ethers.utils.Interface(abi);
+					const contract = new ethers.Contract(
+						RANDOM_CONTRACT_ADDRESS,
+						abi,
+						ethersProvider,
+					);
+					const data = iface.encodeFunctionData("setRandom", [
+						BigInt(randomness),
+					]);
 
+					// Hardcoding gas limit for now so caller does not need correct onchain role
+					const estimatedGas = 43484;
+					// const estimatedGas = await contract.estimateGas.setRandom(
+					//   BigInt(finalRandomnessHex),
+					//   {
+					//     from: PKPETHAddress
+					//   }
+					// )
+					const feeData = await ethersProvider.getFeeData();
+					const nonce = await ethersProvider.getTransactionCount(PKPETHAddress);
+					const unsignedTransaction = {
+						to: RANDOM_CONTRACT_ADDRESS,
+						gasLimit: estimatedGas * 2,
+						nonce,
+						chainId: CHAIN_ID,
+						maxFeePerGas: feeData.maxFeePerGas,
+						maxPriorityFeePerGas: feeData.maxPriorityFeePerGas,
+						type: 2,
+						data,
+					};
+					return JSON.stringify(unsignedTransaction);
+				} catch (error) {
+					return `Error: Could not generate unsigned transaction: ${error.message}`;
+				}
+			},
+		);
+		const unsignedTransaction = JSON.parse(unsignedTransactionResponse);
+		console.log("unsignedTransaction", unsignedTransaction);
 
-    // Hardcoding gas limit for now so caller does not need correct onchain role
-    const estimatedGas = 43484
-    // const estimatedGas = await contract.estimateGas.setRandom(
-    //   BigInt(finalRandomnessHex),
-    //   {
-    //     from: PKPETHAddress
-    //   }
-    // )
+		const serializedTx = ethers.utils.serializeTransaction(unsignedTransaction);
+		const toSign = ethers.utils.arrayify(ethers.utils.keccak256(serializedTx));
+		console.log("toSign", toSign);
+		const litSignature = await Lit.Actions.signAndCombineEcdsa({
+			toSign,
+			publicKey: PKP_PUBLIC_KEY.slice(2),
+			sigName: "signedRandomness",
+		});
 
-    const feeData = await ethersProvider.getFeeData()
-    const unsignedTransaction = {
-      to: RANDOM_CONTRACT_ADDRESS,
-      gasLimit: estimatedGas * 2,
-      nonce: await ethersProvider.getTransactionCount(PKPETHAddress),
-      chainId: CHAIN_ID,
-      maxFeePerGas: feeData.maxFeePerGas,
-      maxPriorityFeePerGas: feeData.maxPriorityFeePerGas,
-      type: 2,
-      data
-    }
+		const jsonSignature = JSON.parse(litSignature);
+		jsonSignature.r = `0x${jsonSignature.r.substring(2)}`;
+		jsonSignature.s = `0x${jsonSignature.s}`;
+		const hexSignature = ethers.utils.joinSignature(jsonSignature);
 
-    const serializedTx = ethers.utils.serializeTransaction(unsignedTransaction)
-    const hashedTxToSign = ethers.utils.arrayify(
-      ethers.utils.keccak256(serializedTx)
-    )
-    const litSignature = await Lit.Actions.signAndCombineEcdsa({
-      toSign: hashedTxToSign,
-      publicKey: PKP_PUBLIC_KEY.slice(2),
-      sigName: "signedRandomness"
-    })
+		const signedTransaction = ethers.utils.serializeTransaction(
+			unsignedTransaction,
+			hexSignature,
+		);
 
-    const jsonSignature = JSON.parse(litSignature)
-    jsonSignature.r = `0x${jsonSignature.r.substring(2)}`
-    jsonSignature.s = `0x${jsonSignature.s}`
-    const hexSignature = ethers.utils.joinSignature(jsonSignature)
+		const recoveredAddress = ethers.utils.recoverAddress(toSign, hexSignature);
+		console.log("Recovered Address:", recoveredAddress);
 
-    const signedTx = ethers.utils.serializeTransaction(
-      unsignedTransaction,
-      hexSignature
-    )
+		Lit.Actions.setResponse({
+			response: JSON.stringify({
+				randomness,
+				unsignedTransaction,
+				signedTransaction,
+				recoveredAddress,
+				timestamp: Date.now(),
+			}),
+		});
+	} catch (error) {
+		Lit.Actions.setResponse({ response: error.message });
+	}
 
-    const recoveredAddress = ethers.utils.recoverAddress(toSign, hexSignature)
-    console.log("Recovered Address:", recoveredAddress)
+	function hexToBytes(hex) {
+		const formattedHex = hex.startsWith("0x") ? hex.slice(2) : hex;
+		if (formattedHex.length % 2 !== 0) {
+			throw new Error("Hex string must have an even length");
+		}
+		const bytes = new Uint8Array(formattedHex.length / 2);
+		for (let i = 0; i < bytes.length; i++) {
+			bytes[i] = Number.parseInt(formattedHex.substr(i * 2, 2), 16);
+		}
+		return bytes;
+	}
+};
 
-    Lit.Actions.setResponse({
-      response: JSON.stringify({
-        randomness: finalRandomnessHex,
-        unsignedTransactionHash,
-        litSignature,
-        signedTx,
-        recoveredAddress,
-        timestamp: Date.now()
-      })
-    })
-  } catch (error) {
-    Lit.Actions.setResponse({ response: error.message })
-  }
-
-  function hexToBytes(hex) {
-    const formattedHex = hex.startsWith("0x") ? hex.slice(2) : hex
-    if (formattedHex.length % 2 !== 0) {
-      throw new Error("Hex string must have an even length")
-    }
-    const bytes = new Uint8Array(formattedHex.length / 2)
-    for (let i = 0; i < bytes.length; i++) {
-      bytes[i] = Number.parseInt(formattedHex.substr(i * 2, 2), 16)
-    }
-    return bytes
-  }
-}
-
-export const litActionCode = `(${_litActionCode.toString()})();`
+export const litActionCode = `(${_litActionCode.toString()})();`;
--- lit.ts
+++ lit.ts
@@ -1,89 +1,91 @@
 import {
-  LitActionResource,
-  LitPKPResource,
-  createSiweMessage,
-  generateAuthSig
-} from "@lit-protocol/auth-helpers"
-import { LIT_ABILITY, LIT_NETWORK, LIT_RPC } from "@lit-protocol/constants"
-import { LitNodeClient } from "@lit-protocol/lit-node-client"
-import * as ethers from "ethers"
-import { litActionCode } from "./action"
-import { pacifica } from "./chain"
+	createSiweMessage,
+	generateAuthSig,
+	LitActionResource,
+	LitPKPResource,
+} from "@lit-protocol/auth-helpers";
+import { LIT_ABILITY, LIT_NETWORK, LIT_RPC } from "@lit-protocol/constants";
+import { LitNodeClient } from "@lit-protocol/lit-node-client";
+import * as ethers from "ethers";
+import { litActionCode } from "./action";
+import { pacifica } from "./chain";
 
-const privateKey = process.env.LIT_PRIVATE_KEY as string
-const RANDOM_CONTRACT_ADDRESS = "0xc75954B9B4Bb4B80883Cf645744612138b7e4870"
+const privateKey = process.env.LIT_PRIVATE_KEY as string;
+const RANDOM_CONTRACT_ADDRESS = "0xc75954B9B4Bb4B80883Cf645744612138b7e4870";
 
 export async function doLit() {
-  const client = new LitNodeClient({
-    litNetwork: LIT_NETWORK.DatilDev,
-    debug: true,
-    alertWhenUnauthorized: true
-  })
-  await client.connect()
+	const client = new LitNodeClient({
+		litNetwork: LIT_NETWORK.DatilDev,
+		debug: true,
+		alertWhenUnauthorized: true,
+	});
+	await client.connect();
 
-  const ethersWallet = new ethers.Wallet(
-    privateKey,
-    new ethers.providers.JsonRpcProvider(LIT_RPC.CHRONICLE_YELLOWSTONE)
-  )
+	const ethersWallet = new ethers.Wallet(
+		privateKey,
+		new ethers.providers.JsonRpcProvider(LIT_RPC.CHRONICLE_YELLOWSTONE),
+	);
 
-  const sessionSigs = await client.getSessionSigs({
-    chain: "pacifica",
-    expiration: new Date(Date.now() + 1000 * 60 * 10).toISOString(), // 10 minutes
-    resourceAbilityRequests: [
-      {
-        resource: new LitActionResource("*"),
-        ability: LIT_ABILITY.LitActionExecution
-      },
-      {
-        resource: new LitPKPResource("*"),
-        ability: LIT_ABILITY.PKPSigning
-      }
-    ],
-    authNeededCallback: async ({
-      uri,
-      expiration,
-      resourceAbilityRequests
-    }) => {
-      const toSign = await createSiweMessage({
-        uri,
-        expiration,
-        resources: resourceAbilityRequests,
-        walletAddress: await ethersWallet.getAddress(),
-        nonce: await client.getLatestBlockhash(),
-        litNodeClient: client
-      })
+	const sessionSigs = await client.getSessionSigs({
+		chain: "pacifica",
+		expiration: new Date(Date.now() + 1000 * 60 * 10).toISOString(), // 10 minutes
+		resourceAbilityRequests: [
+			{
+				resource: new LitActionResource("*"),
+				ability: LIT_ABILITY.LitActionExecution,
+			},
+			{
+				resource: new LitPKPResource("*"),
+				ability: LIT_ABILITY.PKPSigning,
+			},
+		],
+		authNeededCallback: async ({
+			uri,
+			expiration,
+			resourceAbilityRequests,
+		}) => {
+			const toSign = await createSiweMessage({
+				uri,
+				expiration,
+				resources: resourceAbilityRequests,
+				walletAddress: await ethersWallet.getAddress(),
+				nonce: await client.getLatestBlockhash(),
+				litNodeClient: client,
+			});
 
-      return await generateAuthSig({
-        signer: ethersWallet,
-        toSign
-      })
-    }
-  })
-  const { success, response } = await client.executeJs({
-    sessionSigs,
-    code: litActionCode,
-    jsParams: {
-      RANDOM_CONTRACT_ADDRESS: RANDOM_CONTRACT_ADDRESS,
-      RPC_URL: pacifica.rpcUrls.default.http[0],
-      CHAIN_ID: pacifica.id,
-      // PKP Info: {
-      //   publicKey: "0x04c5c14d65f74e8df98dc2af5df40d71695f1b6aaa0c347a3f903c530e47649090069af1ef63f5c58537cb5d955175390ea1af28615bbcac9381c742191f36c0ac",
-      //   ethAddress: "0xaa70631134C9b6c0Bf846faCE0bF04f0238dda8d",
-      //   tokenId: "0x0ad21b2475d5f09cf9861512ea2fe6da0376c97504701b0ede812e2d58d669a5",
-      // }
-      PKP_PUBLIC_KEY: "0x04c5c14d65f74e8df98dc2af5df40d71695f1b6aaa0c347a3f903c530e47649090069af1ef63f5c58537cb5d955175390ea1af28615bbcac9381c742191f36c0ac",
-    }
-  })
-  if (!success) {
-    throw new Error("Failed to execute LIT action")
-  }
-  console.log("response", JSON.parse(response as string))
-  return response
+			return await generateAuthSig({
+				signer: ethersWallet,
+				toSign,
+			});
+		},
+	});
+	const PKP_PUBLIC_KEY =
+		"0x0444ffabb4252caa76daae450bb119b93f02a19354e4052f455d82cf4b159abe0438c188b30a60e94269a3a0767fa35b6e74c5e8953852c89a5cb4bd202a834bfa";
+	const { success, response } = await client.executeJs({
+		sessionSigs,
+		code: litActionCode,
+		jsParams: {
+			RANDOM_CONTRACT_ADDRESS: RANDOM_CONTRACT_ADDRESS,
+			RPC_URL: pacifica.rpcUrls.default.http[0],
+			CHAIN_ID: pacifica.id,
+			// PKP Info: {
+			//   publicKey: "0x04c5c14d65f74e8df98dc2af5df40d71695f1b6aaa0c347a3f903c530e47649090069af1ef63f5c58537cb5d955175390ea1af28615bbcac9381c742191f36c0ac",
+			//   ethAddress: "0xaa70631134C9b6c0Bf846faCE0bF04f0238dda8d",
+			//   tokenId: "0x0ad21b2475d5f09cf9861512ea2fe6da0376c97504701b0ede812e2d58d669a5",
+			// }
+			PKP_PUBLIC_KEY,
+		},
+	});
+	if (!success) {
+		throw new Error("Failed to execute LIT action");
+	}
+	console.log("response", response);
+	return response;
 }
 
 doLit()
-  .then(() => process.exit(0))
-  .catch((e) => {
-    console.error(e)
-    process.exit(1)
-  })
+	.then(() => process.exit(0))
+	.catch((e) => {
+		console.error(e);
+		process.exit(1);
+	});
